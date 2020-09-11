cc79340bbfea3f8d01520489346b27c0fcffbd39
Victoria Landron <viclandron@gmail.com>
2020-09-11 17:50:05 -0400

    wrote fetch call to grab city name

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,11 +2,13 @@ var searchCities = document.querySelector(".search-button");
 var cityList = document.querySelector(".list-cities")
 
 function cityInput() {
+    //check if user inputted a value or empty string
+    
     //grab user input
     var city = document.querySelector("#search-bar").value;
     console.log(city);
 
-    //push city to local storage
+    //push city to local storage as array
     localStorage.setItem("cities", JSON.stringify(city));
 
     //display city on the screen
@@ -18,6 +20,9 @@ function cityInput() {
 
 
     //fetch city info from API
+    fetch("api.openweathermap.org/data/2.5/weather?q=" + 
+    city + 
+    "&api_key=5abd2f3f31f16b1358797c5a21b9e285");
 
     //display city info on screen
 }
