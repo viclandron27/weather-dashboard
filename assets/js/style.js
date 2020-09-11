d82018553ee0c593752795820018bf9c5dd98b22
Victoria Landron <viclandron@gmail.com>
2020-09-11 17:33:15 -0400

    got city to display below search bar and pushed it to local Storage

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,8 +1,25 @@
 var searchCities = document.querySelector(".search-button");
+var cityList = document.querySelector(".list-cities")
 
 function cityInput() {
+    //grab user input
     var city = document.querySelector("#search-bar").value;
     console.log(city);
+
+    //push city to local storage
+    localStorage.setItem("cities", JSON.stringify(city));
+
+    //display city on the screen
+    var cityEl = document.createElement("li");
+    cityEl.setAttribute("class", "search-history")
+    cityEl.innerHTML = city
+    
+    cityList.appendChild(cityEl)
+
+
+    //fetch city info from API
+
+    //display city info on screen
 }
 
 
