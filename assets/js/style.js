a49f3b072e7f9aa937eda6044ba918a7a958e330
Victoria Landron <viclandron@gmail.com>
2020-09-11 22:49:21 -0400

    grab and append city name and temperature

--- assets/js/style.js
+++ assets/js/style.js
@@ -19,12 +19,53 @@ function cityInput() {
     cityList.appendChild(cityEl)
 
 
-    //fetch city info from API
-    fetch("api.openweathermap.org/data/2.5/weather?q=" + 
+    //fetch current forcast city info from API
+    var currentForcast = fetch("http://api.openweathermap.org/data/2.5/weather?q=" + 
     city + 
-    "&api_key=5abd2f3f31f16b1358797c5a21b9e285");
+    "&appid=5abd2f3f31f16b1358797c5a21b9e285&units=imperial")
+    .then(function(currentForcast){
+       return currentForcast.json();
+       
+    })
+    .then(function(currentForcast){
+        console.log(currentForcast);
+        var cityForcastEl = document.querySelector("#city-forcast")
+        cityForcastEl.innerHTML='';
 
-    //display city info on screen
+        console.log(currentForcast.name)
+
+        //create div for current forcast
+        var currentDiv = document.createElement("div");
+        currentDiv.setAttribute("class", "current-forcast");
+
+        cityForcastEl.appendChild(currentDiv);
+
+        //grab city name and display
+        var cityName = document.createElement("h2")  
+        cityName.innerHTML = currentForcast.name
+        currentDiv.append(cityName);
+
+        //grab date
+
+        //grab icon
+
+        //grab temperature
+        var temp = document.createElement("p")
+        temp.innerHTML = "Temperature: " + currentForcast.main.temp + '&deg;F <br>'
+        currentDiv.append(temp);
+        //grab humidity
+
+
+        //cityForcastEl.append()
+    })
+
+    console.log(currentForcast);
+
+    //fetch 5 day forcast
+    //var fiveDayForcast = fetch("http://")
+
+    //display city info on screen (city name, the date, an icon representation 
+    //of weather conditions, the temperature, the humidity, the wind speed, and the UV index)
 }
 
 
